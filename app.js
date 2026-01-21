c0832ad2883da568edb71f3838071eaff249a17c
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-21 14:07:21 +0530

    completed login logout and register

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./src/config/db.js";
 import userModel from "./src/models/userModel.js";
+import postModel from "./src/models/post.js";
 import cookieParser from "cookie-parser";
 import bcrypt, { hash } from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -17,33 +18,67 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/register", (req, res) => {
-  console.log(req.body);
-  res.render("register", { data: req.body });
-});
-
 app.post("/register", async (req, res) => {
   const { username, name, age, email, password } = req.body;
+
+  console.log(req.body);
+
   const exist = await userModel.findOne({ email });
-  if (exist) return res.status(500).json({ message: "user exist already" });
 
-  //   bcrypt.genSalt(10, (err, salt) =>{
+  if (exist) return res.status(400).send("user exist already");
 
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, async (err, salt) => {
-      let user = await userModel.create({
-        username,
-        name,
-        age,
-        email,
-        password: hash,
-      });
-     let token = jwt.sign({ email: email, userid: user._id }, "secretKey");
-    });
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // create user
+
+  const user = await userModel.create({
+    username,
+    name,
+    age,
+    email,
+    password: hashedPassword,
   });
+
+  // generate jwt token
+
+  let token = jwt.sign({ email, userid: user._id }, "secretKey");
+  //setting cookie
+  res.cookie("token", token);
+
+  //
+  res.status(200).send("user registered sucessfully");
 });
 
-app.get("/data", (req, res) => {});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/");
+});
 
 app.listen(3000, () => {
   console.log("serever running at : 3000");
